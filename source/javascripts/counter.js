77959bb4b09f5aceb092669dedd8cc8cdf2fd4e4
LIN HONG <mail.lin.hong@gmail.com>
2020-10-16 18:49:41 +0800

    counter test

--- source/javascripts/counter.js
+++ source/javascripts/counter.js
@@ -4,15 +4,5 @@
         $.get('https://counter.linhong.me/api/add?url=' + url).then((res) => {
             $('#counter').text(res.data.count)
         })
-
-        const pathname = window.location.pathname
-        $.get(
-            'https://counter.linhong.me/api/google/analytics?url=' +
-                pathname +
-                '&startdate=2010-01-01',
-            function (res) {
-                console.log(res.data)
-            }
-        )
     }
 })()
